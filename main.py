5c7131e7237fc12a090c7f27e03277d18a3c4b9e
Scyphers <scyphers.dev@gmail.com>
2023-11-14 23:03:16 +0800

    Add simple item retrieval

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+db = {
+    "apples": 5,
+    "oranges": 3
+}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/{item_id}")
+async def root(item_id: str):
+    if item_id in db:
+        return {item_id: db[item_id]}
+    return {"message": f"No items found for {item_id}"}
