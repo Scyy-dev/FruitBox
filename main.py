4f04532e1d9c990ec36a18158b11a03b74031d6b
Scyphers <scyphers.dev@gmail.com>
2023-11-14 23:30:39 +0800

    Add UUID formatting for points

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
 db = {
-    "player1": 5,
-    "player2": 3,
-    "scyphers": 100000000000
+    "8ef25eca910c4dec86e10ba1ae1afdbd": 100000000000
 }
 
 @app.get("/fruitpoints/{player_uuid}")
-async def get_player_points(player_uuid: str):
+async def get_player_points(
+    player_uuid: Annotated[str, Path(pattern="^[0-9a-fA-F]{32}$")]
+    ):
     if player_uuid in db:
         return {player_uuid: db[player_uuid]}
     return {"message": f"No items found for {player_uuid}"}
