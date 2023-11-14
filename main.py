00d9e85a7d33679b03c19c446fc42b99e6f520b7
Scyphers <scyphers.dev@gmail.com>
2023-11-14 23:20:52 +0800

    change get item to get points

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 db = {
-    "apples": 5,
-    "oranges": 3
+    "player1": 5,
+    "player2": 3,
+    "scyphers": 100000000000
 }
 
-@app.get("/{item_id}")
-async def root(item_id: str):
-    if item_id in db:
-        return {item_id: db[item_id]}
-    return {"message": f"No items found for {item_id}"}
+@app.get("/fruitpoints/{player_uuid}")
+async def get_player_points(player_uuid: str):
+    if player_uuid in db:
+        return {player_uuid: db[player_uuid]}
+    return {"message": f"No items found for {player_uuid}"}
+    
