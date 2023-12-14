ac399692d24141a416037f78ef86388e35b9aae1
Scyphers <scyphers.dev@gmail.com>
2023-12-14 20:08:22 +0800

    experimental test for database connectivity

--- src/app/db/models.py
+++ src/app/db/models.py
@@ -1,8 +1,8 @@
-from typing import Optional
 from sqlmodel import Field, SQLModel
 
-
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class UserBase(SQLModel):
     username: str
     hashed_password: str
+
+class User(UserBase, table=True):
+    id: int = Field(default=None, primary_key=True)
